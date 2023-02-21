6ac04f47ca48c5221f1d38d35f8aea7ba31724f9
gMoneyT <92973063+grace-herbert@users.noreply.github.com>
2023-02-21 22:04:05 +0000

    added a username

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import re
 
+username = input("Please enter a username: ")
 
 print('Please enter a password, it must be at least 11 characters long, have 1 number, 1 Capital letter, 1 lowercase letter and one symbol')
 
 pwd = input('Please enter a password: ')
 
-reenter_pwd = input('Please re-enter a password: ')
+reenter_pwd = input(' Please re-enter a password: ')
+
+pwd_length = len(pwd)
+
+pwd_hidden = '*' * pwd_length
 
 if(len(pwd) >= 11):
   pwd_len = True
@@ -21,12 +26,17 @@ else:format_check = False
 
 if((pwd_len == True) and (pwd_match == True) and (format_check == True)):
   print("The passwords matched! Congratulations!!!")
+  print(f'{username}, the password you entered was {pwd_hidden} and was {pwd_length} Characters long. Your details have been saved.')
+  
 elif not(pwd_len == True and pwd_match == True and format_check == False):
     print("Unfortunately the password is in the wrong format, try again. it must be at least 11 characters long, have 1 number, 1 Capital letter, 1 lowercase letter and one symbol'")
+  
 elif not(pwd_len == True and pwd_match == False and format_check == True):
     print("Passwords did not match, please try again.")
+  
 elif not(pwd_len == False and pwd_match == True and format_check == True):
     print("Please enter a password with at least 11 characters.")
+  
 else:
   print("Incorrect password, please input matching passwords with at least 11 characters long, have 1 number, 1 Capital letter, 1 lowercase letter and one symbol")
 
